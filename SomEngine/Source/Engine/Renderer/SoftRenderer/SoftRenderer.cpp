a2052d25963786f3aa777f9c9cc859212fb4983f
JeongSeokPark <wjdtjr2676@gmail.com>
2019-07-31 03:06:11 +0900

    Update Line

--- SomEngine/Source/Engine/Renderer/SoftRenderer/SoftRenderer.cpp
+++ SomEngine/Source/Engine/Renderer/SoftRenderer/SoftRenderer.cpp
@@ -2,113 +2,15 @@
 
 #include "SoftRenderer.h"
 
-// SomWorks :D // 인터넷 레퍼런스
-void Reference(Point2D v1, Point2D v2)
-{
-	int dx, dy;
-	int p_value;
-	int inc_minus;
-	int inc_plus;
-	int inc_value;
-	int ndx;
-
-	dx = abs(v2.x - v1.x);
-	dy = abs(v2.y - v1.y);
-
-	//변화폭을 따져 완만한 좌표를 기준으로 점을 찍는다.
-	if (dy <= dx)
-	{
-		// y 보다 x 변화폭이 클 경우
-		inc_minus = 2 * dy;
-		inc_plus = 2 * (dy - dx);
-
-		//선 x 점을 작은 수부터 그려나가기 위해.
-		//Bresenham 공식은 다음점에 대한 위치를 잡는 것이기 때문에도.
-
-		if (v2.x < v1.x)
-		{
-			ndx = v1.x;
-			v1.x = v2.x;
-			v2.x = ndx;
-
-			ndx = v1.y;
-			v1.y = v2.y;
-			v2.y = ndx;
-		}
-
-		//x 값에 따른 y값 변화값 고르기
-		if (v1.y < v2.y) {
-			inc_value = 1;
-		}
-		else {
-			inc_value = -1;
-		}
-
-		FSomDrawLibrary::DrawPixel(v1.x, v1.y);
-
-		//처음 판단 값
-		p_value = 2 * dy - dx;
-
-		for (ndx = v1.x; ndx < v2.x; ++ndx)
-		{
-			if (0 > p_value) {
-				p_value += inc_minus;
-			}
-			else {
-				p_value += inc_plus;
-				v1.y += inc_value;
-			}
-		
-			FSomDrawLibrary::DrawPixel(ndx, v1.y);
-		}
-	}
-	else
-	{
-		inc_minus = 2 * dx;
-		inc_plus = 2 * (dx - dy);
-
-		if (v2.y < v1.y) {
-			ndx = v1.y;
-			v1.y = v2.y;
-			v2.y = ndx;
-
-			ndx = v1.x;
-			v1.x = v2.x;
-			v2.x = ndx;
-		}
-
-		if (v1.x < v2.x) {
-			inc_value = 1;
-		}
-		else {
-			inc_value = -1;
-		}
-		FSomDrawLibrary::DrawPixel(v1.x, v1.y);
-
-		p_value = 2 * dx - dy;
-
-		for (ndx = v1.y; ndx < v2.y; ++ndx)
-		{
-			if (0 > p_value)
-			{
-				p_value += inc_minus;
-			}
-			else
-			{
-				p_value += inc_plus;
-				v1.x += inc_value;
-			}
-
-			FSomDrawLibrary::DrawPixel(v1.x, ndx);
-		}
-	}
-}
-
 void UpdateFrame(void)
 {
 	// Draw
 	SomFramework_SR::GetInstance()->SetColor(255, 255, 255); // SetColor(255, 0, 0);
 	// FSomDrawLibrary::DrawPixel(0, 0);
+
+	// SomWorks :D // 십자선
+	FSomDrawLibrary::DrawLine_BresenhamAlgorithm(Point2D(0 - SomWidth / 2, 0), Point2D(0 + SomWidth / 2, 0));
+	FSomDrawLibrary::DrawLine_BresenhamAlgorithm(Point2D(0, 0 + SomHeight / 2), Point2D(0, 0 - SomHeight / 2));
 
 	Point2D p1;
 	p1.x = 0;
@@ -117,10 +19,12 @@ void UpdateFrame(void)
 	Point2D p2;
 	p2.x = -100;
 	p2.y = 100;
+
+	// FSomDrawLibrary::DrawLine(p1, p2);
+	// FSomDrawLibrary::DrawCircle(p1, 10);
+
+	FSomDrawLibrary::DrawTriangle(FVector2D(-100, -100), FVector2D(-50, 100), FVector2D(80, -50));
 	
-	//Reference(p1, p2);
-	FSomDrawLibrary::DrawLine(p1, p2);
-	//FSomDrawLibrary::DrawCircle(p1, 10);
 	// =================================================================
 	// SomWorks :D // Debug
 
--- SomEngine/Source/Engine/SomLibraries/Draw/SomDrawLibrary.cpp
+++ SomEngine/Source/Engine/SomLibraries/Draw/SomDrawLibrary.cpp
@@ -58,23 +58,114 @@ void FSomDrawLibrary::DrawLine_BresenhamAlgorithm(Point2D Point1, Point2D Point2
 
 	int dx, dy = 0;
 
+	int p_value;
+	int inc_minus;
+	int inc_plus;
+	int inc_value;
+	int ndx;
+
 	dx = abs(Point2.x - Point1.x);
 	dy = abs(Point2.y - Point1.y);
 
+	//변화폭을 따져 완만한 좌표를 기준으로 점을 찍는다.
 	if (dy <= dx)
 	{
+		// y 보다 x 변화폭이 클 경우
+		inc_minus = 2 * dy;
+		inc_plus = 2 * (dy - dx);
 
+		//선 x 점을 작은 수부터 그려나가기 위해.
+		//Bresenham 공식은 다음점에 대한 위치를 잡는 것이기 때문에도.
+
+		if (Point2.x < Point1.x)
+		{
+			ndx = Point1.x;
+			Point1.x = Point2.x;
+			Point2.x = ndx;
+
+			ndx = Point1.y;
+			Point1.y = Point2.y;
+			Point2.y = ndx;
+		}
+
+		//x 값에 따른 y값 변화값 고르기
+		if (Point1.y < Point2.y) {
+			inc_value = 1;
+		}
+		else {
+			inc_value = -1;
+		}
+
+		DrawPixel(Point1.x, Point1.y);
+
+		//처음 판단 값
+		p_value = 2 * dy - dx;
+
+		for (ndx = Point1.x; ndx < Point2.x; ++ndx)
+		{
+			if (0 > p_value) {
+				p_value += inc_minus;
+			}
+			else {
+				p_value += inc_plus;
+				Point1.y += inc_value;
+			}
+
+			DrawPixel(ndx, Point1.y);
+		}
 	}
 	else
 	{
+		inc_minus = 2 * dx;
+		inc_plus = 2 * (dx - dy);
 
+		if (Point2.y < Point1.y) {
+			ndx = Point1.y;
+			Point1.y = Point2.y;
+			Point2.y = ndx;
+
+			ndx = Point1.x;
+			Point1.x = Point2.x;
+			Point2.x = ndx;
+		}
+
+		if (Point1.x < Point2.x) {
+			inc_value = 1;
+		}
+		else {
+			inc_value = -1;
+		}
+		DrawPixel(Point1.x, Point1.y);
+
+		p_value = 2 * dx - dy;
+
+		for (ndx = Point1.y; ndx < Point2.y; ++ndx)
+		{
+			if (0 > p_value)
+			{
+				p_value += inc_minus;
+			}
+			else
+			{
+				p_value += inc_plus;
+				Point1.x += inc_value;
+			}
+
+			DrawPixel(Point1.x, ndx);
+		}
 	}
 }
 
 // SomWorks :D // 삼각형 그리기
 void FSomDrawLibrary::DrawTriangle(FVector2D a, FVector2D b, FVector2D c)
 {
+	Point2D a1 = Point2D(static_cast<int>(a.x), static_cast<int>(a.y));
+	Point2D b1 = Point2D(static_cast<int>(b.x), static_cast<int>(b.y));
+	Point2D c1 = Point2D(static_cast<int>(c.x), static_cast<int>(c.y));
 
+	DrawLine_BresenhamAlgorithm(a1, b1);
+	DrawLine_BresenhamAlgorithm(b1, c1);
+	DrawLine_BresenhamAlgorithm(c1, a1);
 }
 
 // SomWorks :D // 원 그리기
